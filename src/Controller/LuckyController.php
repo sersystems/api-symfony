05e2ad77dd25b17b3c28122be8af6c35e08280f3
Sergio Regalado Alessi <serdigital@live.com.ar>
2022-01-15 14:07:01 -0300

    Config v1

---
+++ src/Controller/LuckyController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class LuckyController extends AbstractController
+{
+    /**
+     * @Route("/api/number/{max}", name="app_lucky_number")
+     */
+    public function number(int $max): Response
+    {
+        $number = random_int(0, $max);
+
+        return new Response(
+            '<html><body>Lucky number: '.$number.'</body></html>'
+        );
+    }
+}
